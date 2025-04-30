4cf2cb1bb72dd67331cdf1e7c2e571e447e5fd9f
IcySpar <victorasielrodriguez@gmail.com>
2025-04-30 09:36:41 -0700

    feat: create first resource

--- app.js
+++ app.js
@@ -3,12 +3,80 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 
+const { v4: uuidv4 } = require('uuid');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.end('Hello World!')
-})
 
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+}
+
+
+// app.get('/', (req, res) => {
+//   return res.send('Received a GET HTTP method')
+// });
+
+// app.post('/', (req, res) => {
+//   return res.send('Received a POST HTTP method')
+// });;
+
+// app.put('/users/:userId', (req, res) => {
+//   return res.send(`Received a PUT HTTP method on user ${req.params.userId}`)
+// });
+
+// app.delete('/users/:userId', (req, res) => {
+//   return res.send(`Received a DELETE HTTP method on user ${req.params.userId}`)
+// });
+
+app.get('/users', (req, res) => {
+  return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+
+
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+  };
+
+  messages[id] = message;
+  return res.send(message);
+});
+
+app.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
+  console.log(`Listening on port ${PORT}`);
 });
